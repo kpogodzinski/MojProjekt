5fede53513aa567203566e2992a001d501685ce5
kpogodzinski <kacperpogodzinski@spoko.pl>
2024-03-25 10:28:30 +0100

    silnia() zwraca teraz ull. Dodano obsluge funkcji do menu.

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int silnia(int n) {
+unsigned long long silnia(int n) {
 	if (n == 0)
 		return 1;
 	if (n > 1)
@@ -11,17 +11,26 @@ int silnia(int n) {
 }
 
 int main() {
-    int a, b;
-    cout << "Podaj liczbe calkowita ";
-    cin >> a >> b;
+//    int a, b;
+//    cout << "Podaj liczbe calkowita ";
+//    cin >> a >> b;
     int wyjscie;
     do {
         cout << endl;
         cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
+        cout << "1. Silnia" << endl;
         cout << "0. Wyjscie" << endl;
         cin >> wyjscie;
+        
+        if (wyjscie == 1) {
+        	int n;
+        	cout<<"\nPodaj liczbe: ";
+        	cin>>n;
+        	if (n < 0) cout<<"Niepoprawny argument."<<endl;
+        	else cout<<n<<"! = "<<silnia(n)<<endl;
+		}
     } while(wyjscie != 0);
     return 0;
 }
