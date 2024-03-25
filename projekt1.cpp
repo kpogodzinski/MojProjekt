8fd0f6ce0c9d548342bf4090c824e8fcc81bf02f
kpogodzinski <kacperpogodzinski@spoko.pl>
2024-03-25 10:36:44 +0100

    Dodanie funkcji do sprawdzania czy podana liczba jest pierwsza.

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+bool czyPierwsza(int n) {
+	if (n == 0 || n == 1)
+		return false;
+	if (n == 2)
+		return true;
+	
+	for (int i=3; i*i<=n; i+=2) {
+		if (n%i == 0)
+			return false;
+	}
+	
+	return true;
+}
+
 int main() {
     int a, b;
     cout << "Podaj liczbe calkowita ";
